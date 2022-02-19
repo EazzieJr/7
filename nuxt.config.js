b76c453e9e1d64b842bc7f56681241cc6199abf0
Ganiyu Idris <golamide27@gmail.com>
2022-02-19 07:36:44 +0100

    First commit

---
+++ nuxt.config.js
@@ -0,0 +1,74 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+  server: {
+    host: process.env.HOST || '0.0.0.0',
+    port: process.env.PORT || 8080,
+  },
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'emishio',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/tailwind/tailwind.css',
+    // '@/assets/scss/main.scss'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // Nuxt-Image
+    // '@nuxt/image',
+    // Google fonts
+    '@nuxtjs/google-fonts',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+    '@nuxt/postcss8',
+    // Font awesome
+    // '@nuxtjs/fontawesome'
+  ],
+
+  googleFonts: {
+    /* module options */
+    families: {
+      Montserrat: [ 400 ]
+    }
+  },
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: ["gsap"],
+
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <section class="hero relative w-full h-screen bg-center bg-cover bg-no-repeat" style="background-image: url('/svg/Hero.svg')">
+      <div class="dropdown abolute w-full h-full flex justify-center items-center bg-black">
+        <div class="dropdown-container flex flex-col justify-center items-center">
+          <ul class="font-montserrat text-3xl text-white text-center space-y-4">
+            <li><a href="">About</a></li>
+            <li><a href="">Work</a></li>
+            <li><a href="">Articles</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+      
+      <div class="container mx-auto px-7">
+        <nav>
+          <div class="nav-container py-10 flex justify-between items-center z-50">
+            <div class="left-container">
+              <div class="logo">
+                <Logo />
+              </div>
+            </div>
+
+            <div class="right-container">
+              <div class="menu-burger space-y-1.5">
+                <div class="w-7 min-h-[1.5px] rounded bg-white"></div>
+                <div class="w-7 min-h-[1.5px] rounded bg-white"></div>
+                <div class="w-7 min-h-[1.5px] rounded bg-white"></div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        
+        <div class="hero-container">
+
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+.nue-regular {
+  font-family: 'Neue Metana Regular';
+  font-weight: 400;
+}
+
+.nue-bold {
+  font-family: 'Neue Metana Bold';
+  font-weight: 700;
+}
+
+@font-face {
+  font-family: 'Neue Metana Regular';
+  src: url('/font/NeueMetana-Regular.otf');
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Neue Metana Bold';
+  src: url('/font/NeueMetana-Bold.otf');
+  font-weight: 700;
+  font-style: normal;
+  font-display: swap;
+}
+</style>
